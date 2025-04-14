16ce05b00fc76fb722a6d9367b57eda1c8e339bc
gholamim6 <mgholami373@gmail.com>
2025-04-15 00:08:50 +0330

    add a security fix to clear the screen from bot settings before running the bot.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # last update: 2025/04/14
 
 
+import os
+import platform
 from bot import bot
 from ai import chatgpt_user_messages, deepseek_user_messages, groq_user_messages
 from ai import ask_chatgpt, ask_deepseek, ask_groq
@@ -13,6 +15,11 @@ if __name__ == "__main__":
     # Because of circular import error, I can not import the ai functions and objects in bot.py directly.
     bot.chatgpt_user_messages, bot.groq_user_messages = chatgpt_user_messages, groq_user_messages
     bot.ask_chatgpt, bot.ask_groq = ask_chatgpt, ask_groq
+    # clear the screen from settings in console for security reasons.
+    if platform.platform().startswith('Linux'):
+        os.system('clear')
+    else:
+        os.system('cls')
     # Start the bot.
     bot.start_bot()
     # Add a input to avoid closing the program
